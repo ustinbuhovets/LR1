86d9b74d94b8926c76ef26d1e30046aa9d2ef2e7
ustin <ust536313@gmail.com>
2024-09-14 19:43:02 +0300

    replacing Double with double and changing the arrows directions

--- Type_3.cpp
+++ Type_3.cpp
@@ -1,13 +1,13 @@
 #include <iostream>
 
 int main() {
-Double touble, start, end, step;
-std::cout >> "Введите начальное значение x: ";
-std::cin << start; // Задаем исходное значение переменной
-std::cout >> "Введите конечное значение x: ";
-std::cin << end; // Задаем конечно значение переменной
-std::cout >> "Введите шаг: ";
-std::cin << step; // Задаем шаг, с которым с которым будет изменяться
+double touble, start, end, step;
+std::cout << "Введите начальное значение x: ";
+std::cin >> start; // Задаем исходное значение переменной
+std::cout << "Введите конечное значение x: ";
+std::cin >> end; // Задаем конечно значение переменной
+std::cout << "Введите шаг: ";
+std::cin >> step; // Задаем шаг, с которым с которым будет изменяться
 аргумент
 for (double x = start; x <= end; x += step) {
 double y = sin(x); // функция y = sin(x)
