85eb3a4dfb9b601ae9538220272915ecc09ef465
ustin <ust536313@gmail.com>
2024-09-14 19:44:54 +0300

    fixing comments, adding tabs and paragraphs

--- Type_3.cpp
+++ Type_3.cpp
@@ -1,18 +1,21 @@
 #include <iostream>
 
 int main() {
-double touble, start, end, step;
-std::cout << "Введите начальное значение x: ";
-std::cin >> start; // Задаем исходное значение переменной
-std::cout << "Введите конечное значение x: ";
-std::cin >> end; // Задаем конечно значение переменной
-std::cout << "Введите шаг: ";
-std::cin >> step; // Задаем шаг, с которым с которым будет изменяться
-аргумент
-for (double x = start; x <= end; x += step) {
-double y = sin(x); // функция y = sin(x)
-std::cout << x << " | " << y << std::endl; // Выводим значения
-функции для каждого аргумента (x | y)
-}
-return 0;
+    
+    double touble, start, end, step;
+
+    std::cout << "Введите начальное значение x: ";
+    std::cin >> start; // Задаем исходное значение переменной
+    std::cout << "Введите конечное значение x: ";
+    std::cin >> end; // Задаем конечно значение переменной
+    std::cout << "Введите шаг: ";
+    std::cin >> step; // Задаем шаг, с которым с которым будет изменяться аргумент
+
+    for (double x = start; x <= end; x += step) {
+        double y = sin(x); // функция y = sin(x)
+        std::cout << x << " | " << y << std::endl; // Выводим значения функции для каждого аргумента (x | y)
+    }
+
+    return 0;
+
 }
